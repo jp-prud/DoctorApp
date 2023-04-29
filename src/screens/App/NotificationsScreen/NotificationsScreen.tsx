2444a502fb2e40131f38a2c8d9ade2b14bec749a
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-04-29 00:44:22 -0300

    feat: create notification screen

---
+++ src/screens/App/NotificationsScreen/NotificationsScreen.tsx
@@ -0,0 +1,75 @@
+import {ListRenderItemInfo, FlatList, View} from 'react-native';
+import {Heading} from '@components/atomic/Heading';
+
+import {Container} from './styles';
+import {notificationsList} from 'src/mocks/Notifications';
+
+import {Card} from './components/Card';
+import {Text} from '@components/atomic/Text';
+
+export type Notification = {
+  title: string;
+  description: string;
+};
+
+export type NotificationGroup = {
+  date: string;
+  notifications: Notification[];
+};
+
+const FORMATED_DATE_OPTIONS = {
+  weekday: 'long',
+  day: '2-digit',
+  month: 'long',
+  year: 'numeric',
+};
+
+export function NotificationsScreen() {
+  function Separator({height = 24}: {height?: number}) {
+    return <View style={{height: height}} />;
+  }
+
+  function renderNotificationDayContainer({
+    item: notificationDayList,
+  }: ListRenderItemInfo<NotificationGroup>) {
+    const {date, notifications} = notificationDayList;
+
+    const currentFormatedDate = new Date(Number(date)).toLocaleString(
+      'pt-BR',
+      FORMATED_DATE_OPTIONS,
+    );
+
+    return (
+      <View>
+        <Text style={{marginBottom: 8}} weight='500'>{currentFormatedDate}</Text>
+
+        <FlatList
+          data={notifications}
+          keyExtractor={({title}) => `${title}-${Math.random()}`}
+          renderItem={renderNotificationCard}
+          ItemSeparatorComponent={<Separator height={20} />}
+        />
+      </View>
+    );
+  }
+
+  function renderNotificationCard({
+    item: notificationData,
+  }: ListRenderItemInfo<Notification>) {
+    return <Card {...notificationData} />;
+  }
+
+  return (
+    <Container>
+      <Heading title="Notificações" size="XL" hasBackButton />
+
+      <FlatList
+        data={notificationsList}
+        keyExtractor={({date}: NotificationGroup) => date}
+        renderItem={renderNotificationDayContainer}
+        style={{marginTop: 24, marginBottom: 8}}
+        ItemSeparatorComponent={<Separator />}
+      />
+    </Container>
+  );
+}
---
+++ src/screens/App/NotificationsScreen/components/Card/index.tsx
@@ -0,0 +1,30 @@
+import {Image} from 'react-native';
+
+import {Text} from '@components/atomic/Text';
+import {Notification} from '../../NotificationsScreen';
+
+import {Container, Details} from './styles';
+
+export function Card({title, description}: Notification) {
+  return (
+    <Container>
+      <Image
+        source={{
+          uri: 'http://via.placeholder.com/92x92.png',
+          width: 92,
+        }}
+        style={{borderRadius: 8, height: 92}}
+      />
+
+      <Details>
+        <Text size="MD" weight="800">
+          {title}
+        </Text>
+
+        <Text size="SM" weight="400" >
+          {description}
+        </Text>
+      </Details>
+    </Container>
+  );
+}
