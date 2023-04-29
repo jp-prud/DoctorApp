1bfbefe3cad2915b843d2437eb1a202c70358875
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-04-29 00:41:01 -0300

    chore: add new dependecies and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  rules: [
-    
-  ]
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
+
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.DoctorApp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
