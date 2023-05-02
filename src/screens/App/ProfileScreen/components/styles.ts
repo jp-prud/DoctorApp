5be365c71347aa2d3f41df84523b269c81cc2672
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-02 00:19:30 -0300

    styles: profile card screens

---
+++ src/screens/App/ProfileScreen/components/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  height: 64px;
+  padding: 16px;
+  background: #fff;
+  border-radius: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ButtonWrapper = styled.View`
+  transform: scale(-1);
+`
