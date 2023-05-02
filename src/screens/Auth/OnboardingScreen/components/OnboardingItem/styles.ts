546a0324968d5d8501c64222f2fa430bd9709428
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-01 23:35:18 -0300

    styles: onboarding screen

---
+++ src/screens/Auth/OnboardingScreen/components/OnboardingItem/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
+
+export const Details = styled.View`
+  flex: 0.3;
+  gap: 8px;
+  align-items: center;
+`;
---
+++ src/screens/Auth/OnboardingScreen/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+import ViewWrapper from '@components/atomic/ViewWrapper';
+
+export const Container = styled(ViewWrapper)`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding-left: 0;
+  padding-right: 0;
+`;
+
+export const NavigationContainer = styled.View`
+  flex: 1;
+`;
