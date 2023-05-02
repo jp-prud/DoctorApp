362c15116d8026acf38c84f536fc923dfc7e8857
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-01 23:36:02 -0300

    feat: input text component

--- src/components/atomic/Input.ts
+++ src/components/atomic/Input.ts
@@ -1,19 +1,27 @@
-import styled, { css } from "styled-components/native";
+import styled, {css} from 'styled-components/native';
 
-import { TextInputProps } from 'react-native'
+import {TextInputProps} from 'react-native';
 
-export default styled.TextInput<TextInputProps>`
+export default styled.TextInput<TextInputProps & FieldErrorProps>`
   align-items: center;
   background: #fff;
   border-radius: 8px;
 
-  ${({theme}) => css`
-    height: ${theme.SIZE['5X']}px;
-    padding-left: ${theme.SIZE.SM}px;
-    padding-right: ${theme.SIZE.SM}px;
-    border: 1px solid ${theme.COLORS.GRAY_200};
-    font-size: ${theme.SIZE.SM}px;
-    font-family: ${theme.FONT_FAMILY.MEDIUM};
-    color: #666;
-  `}
+  ${({theme}) =>
+    css`
+      height: ${theme.SIZE['5X']}px;
+      border: 1px solid ${theme.COLORS.GRAY_200};
+      padding-left: ${theme.SIZE.SM}px;
+      padding-right: ${theme.SIZE.SM}px;
+      font-size: ${theme.SIZE.SM}px;
+      font-family: ${theme.FONT_FAMILY.MEDIUM};
+      color: ${theme.COLORS.GRAY_500};
+    `}
+
+  ${({theme, error}) =>
+    error &&
+    css`
+      border-color: ${theme.COLORS.RED};
+      color: ${theme.COLORS.RED};
+    `}
 `;
