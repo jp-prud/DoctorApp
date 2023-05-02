52a31ce0e6acbc3faab4172df8fd8f9d04426578
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-02 00:23:47 -0300

    feat: initializated history screen

---
+++ src/screens/App/HistoryScreen/HistoryScreen.tsx
@@ -0,0 +1,10 @@
+import {Heading} from '@components/atomic/Heading';
+import ViewWrapper from '@components/atomic/ViewWrapper';
+
+export function HistoryScreen() {
+  return (
+    <ViewWrapper>
+      <Heading title="Histórico"/>
+    </ViewWrapper>
+  );
+}
