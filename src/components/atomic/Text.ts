9a9cc1cd8da41d66b76fa9e9255f95a8ea5df44c
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-01 23:36:46 -0300

    styçes: text component size and colors

--- src/components/atomic/Text.ts
+++ src/components/atomic/Text.ts
@@ -1,9 +1,10 @@
+import { Size } from '@types/styled';
 import styled, {css} from 'styled-components/native';
 
 interface TextProps {
   weight?: '200' | '300' | '400' | '500' | '600' | '700' | '800';
   color?: string;
-  size?: number;
+  size?: Size | number;
   opacity?: number;
   align?: 'left' | 'center' | 'right';
 }
@@ -14,7 +15,7 @@ export const Text = styled.Text<TextProps>`
       weight ? `Urbanist_${weight}` : 'Urbanist_400'};
 
     color: ${({color}: TextProps) =>
-      (color && theme.COLORS[color]) || '#44404f'};
+      (color && theme.COLORS[color]) || theme.COLORS.GRAY_500};
 
     font-size: ${({size}: TextProps) =>
       size ? `${theme.SIZE[size]}px` : '16px'};
