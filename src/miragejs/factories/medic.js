c3850d77677deeb30b0afe1df2881db0dac97599
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-13 23:44:01 -0300

    feat: add miragejs dependencies

---
+++ src/miragejs/factories/medic.js
@@ -0,0 +1,17 @@
+/*
+ * Mirage JS guide on Factories: https://miragejs.com/docs/data-layer/factories
+ */
+import { Factory } from 'miragejs';
+
+/*
+ * Faker Github repository: https://github.com/Marak/Faker.js#readme
+ */
+import { faker } from '@faker-js/faker';
+
+export default {
+  medic: Factory.extend({
+    name() {
+      return faker.name().fullName();
+    },
+  }),
+};
