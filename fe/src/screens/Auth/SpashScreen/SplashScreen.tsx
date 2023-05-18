823505ff93d9f471b048552c5e3b584f15d726a4
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-05-18 15:53:48 -0300

    feat: splash screen

---
+++ fe/src/screens/Auth/SpashScreen/SplashScreen.tsx
@@ -0,0 +1,16 @@
+import { Container } from "./styles";
+
+import { Text } from "../../../components/atomic/Text";
+
+export function SplashScreen() {
+  return (
+    <Container>
+      <Text>
+        Doctor
+        <Text>APP</Text>
+      </Text>
+
+      <Text>Atendimento médico facilitado.</Text>
+    </Container>
+  );
+}
---
+++ fe/src/screens/Auth/SpashScreen/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: ${({ theme }) => theme.COLORS.BLUE};
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`;
